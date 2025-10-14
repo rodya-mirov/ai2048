9fdacca9272fec4a4b7674dbf76895cf177be5b4
Richard Rast <richard.rast@trinetx.com>
2025-10-14 13:48:06 -0400

    feat: actual implementation

--- src/game_structs.rs
+++ src/game_structs.rs
@@ -1,5 +1,8 @@
 use crate::game_traits::{AddRandomPiece, FullGame};
-use rand::{Rng, RngCore, SeedableRng};
+use rand::{Rng, SeedableRng};
+
+#[cfg(test)]
+mod tests;
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub struct GameState<const N: usize> {
@@ -23,18 +26,49 @@ pub enum MoveError {
 }
 
 impl<const N: usize> GameState<N> {
-    // TODO: unit tests, including the order of operations, which is important
     fn left(&self) -> Self {
         let mut out = *self;
 
-        for x in 1..N {
-            for y in 0..N {
-                if out.grid[y][x - 1] == out.grid[y][x] {
-                    out.grid[y][x - 1] <<= 1;
-                    out.grid[y][x] = 0;
-                } else if out.grid[y][x - 1] == 0 {
-                    out.grid[y][x - 1] = out.grid[y][x];
-                    out.grid[y][x] = 0;
+        // (indexed by y, output is an x-value) -- cannot merge in a row twice in the same space,
+        // so this tracks the last merge point, if any. Merges can happen at this point but not
+        // earlier.
+        let mut merge_limits: [usize; N] = [0; N];
+
+        for y in 0 .. N {
+            // from left to right, attempt to move each piece left until it can't anymore,
+            // except that you cannot merge the same square more than once
+            for x in 1 .. N {
+                let val = out.grid[y][x];
+                if val == 0 {
+                    continue;
+                }
+
+                // now slide it left continually until it hits a wall or the merge limit
+                let mut new_x = x - 1;
+
+                loop {
+                    if out.grid[y][new_x] == 0 {
+                        // blank space; move and continue
+                        out.grid[y][new_x] = val;
+                        out.grid[y][new_x + 1] = 0;
+                    } else if out.grid[y][new_x] == val {
+                        // merge; do the merge and stop the train
+                        out.grid[y][new_x] = val << 1;
+                        out.grid[y][new_x + 1] = 0;
+                        merge_limits[y] = new_x + 1;
+                        break;
+                    } else {
+                        // we hit a wall, stop moving
+                        break;
+                    }
+
+                    // if we're at the end of the line, stop
+                    if new_x == merge_limits[y] {
+                        break;
+                    }
+
+                    // otherwise set up the next loop
+                    new_x -= 1;
                 }
             }
         }
@@ -42,18 +76,47 @@ impl<const N: usize> GameState<N> {
         out
     }
 
-    // TODO: unit tests, including the order of operations, which is important
     fn up(&self) -> Self {
         let mut out = *self;
 
-        for y in 1..N {
-            for x in 0..N {
-                if out.grid[y - 1][x] == out.grid[y][x] {
-                    out.grid[y - 1][x] <<= 1;
-                    out.grid[y][x] = 0;
-                } else if out.grid[y - 1][x] == 0 {
-                    out.grid[y - 1][x] = out.grid[y][x];
-                    out.grid[y][x] = 0;
+        // (indexed by x, output is a y-value) -- cannot merge in a column twice in the same space,
+        // so this tracks the last merge point, if any. Merges can happen at this point but not
+        // earlier.
+        let mut merge_limits: [usize; N] = [0; N];
+
+        for x in 0 .. N {
+            // from top to bottom, attempt to move each piece upward until it can't anymore,
+            // except that you cannot merge the same square more than once
+            for y in 1 .. N {
+                let val = out.grid[y][x];
+                if val == 0 {
+                    continue;
+                }
+
+                // now slide it up continually until it hits a wall or the merge limit
+                let mut new_y = y - 1;
+
+                loop {
+                    if out.grid[new_y][x] == 0 {
+                        out.grid[new_y][x] = val;
+                        out.grid[new_y+1][x] = 0;
+                    } else if out.grid[new_y][x] == val {
+                        out.grid[new_y][x] = val << 1;
+                        out.grid[new_y+1][x] = 0;
+                        merge_limits[x] = new_y + 1;
+                        break;
+                    } else {
+                        // we hit a wall, stop moving
+                        break;
+                    }
+
+                    // if we're at the end of the line, stop
+                    if new_y == merge_limits[x] {
+                        break;
+                    }
+
+                    // otherwise set up the next loop
+                    new_y -= 1;
                 }
             }
         }
@@ -61,18 +124,49 @@ impl<const N: usize> GameState<N> {
         out
     }
 
-    // TODO: unit tests, including the order of operations, which is important
     fn right(&self) -> Self {
         let mut out = *self;
 
-        for x in (0..N - 1).rev() {
-            for y in 0..N {
-                if out.grid[y][x + 1] == out.grid[y][x] {
-                    out.grid[y][x + 1] <<= 1;
-                    out.grid[y][x] = 0;
-                } else if out.grid[y][x + 1] == 0 {
-                    out.grid[y][x + 1] = out.grid[y][x];
-                    out.grid[y][x] = 0;
+        // (indexed by y, output is an x-value) -- cannot merge in a row twice in the same space,
+        // so this tracks the last merge point, if any. Merges can happen at this point but not
+        // earlier.
+        let mut merge_limits: [usize; N] = [N-1; N];
+
+        for y in 0 .. N {
+            // from right to left, attempt to move each piece right until it can't anymore,
+            // except that you cannot merge the same square more than once
+            for x in (0 .. N-1).rev() {
+                let val = out.grid[y][x];
+                if val == 0 {
+                    continue;
+                }
+
+                // now slide it right continually until it hits a wall or the merge limit
+                let mut new_x = x + 1;
+
+                loop {
+                    if out.grid[y][new_x] == 0 {
+                        // blank space; move and continue
+                        out.grid[y][new_x] = val;
+                        out.grid[y][new_x - 1] = 0;
+                    } else if out.grid[y][new_x] == val {
+                        // merge; do the merge and stop the train
+                        out.grid[y][new_x] = val << 1;
+                        out.grid[y][new_x - 1] = 0;
+                        merge_limits[y] = new_x - 1;
+                        break;
+                    } else {
+                        // we hit a wall, stop moving
+                        break;
+                    }
+
+                    // if we're at the end of the line, stop
+                    if new_x == merge_limits[y] {
+                        break;
+                    }
+
+                    // otherwise set up the next loop
+                    new_x += 1;
                 }
             }
         }
@@ -80,18 +174,47 @@ impl<const N: usize> GameState<N> {
         out
     }
 
-    // TODO: unit tests, including the order of operations, which is important
     fn down(&self) -> Self {
         let mut out = *self;
 
-        for y in (0..N - 1).rev() {
-            for x in 0..N {
-                if out.grid[y + 1][x] == out.grid[y][x] {
-                    out.grid[y + 1][x] <<= 1;
-                    out.grid[y][x] = 0;
-                } else if out.grid[y + 1][x] == 0 {
-                    out.grid[y + 1][x] = out.grid[y][x];
-                    out.grid[y][x] = 0;
+        // (indexed by x, output is a y-value) -- cannot merge in a column twice in the same space,
+        // so this tracks the last merge point, if any. Merges can happen at this point but not
+        // earlier.
+        let mut merge_limits: [usize; N] = [N-1; N];
+
+        for x in 0 .. N {
+            // from bottom to top, attempt to move each piece downward until it can't anymore,
+            // except that you cannot merge the same square more than once
+            for y in (0 .. N-1).rev() {
+                let val = out.grid[y][x];
+                if val == 0 {
+                    continue;
+                }
+
+                // now slide it down continually until it hits a wall or the merge limit
+                let mut new_y = y + 1;
+
+                loop {
+                    if out.grid[new_y][x] == 0 {
+                        out.grid[new_y][x] = val;
+                        out.grid[new_y-1][x] = 0;
+                    } else if out.grid[new_y][x] == val {
+                        out.grid[new_y][x] = val << 1;
+                        out.grid[new_y-1][x] = 0;
+                        merge_limits[x] = new_y - 1;
+                        break;
+                    } else {
+                        // we hit a wall, stop moving
+                        break;
+                    }
+
+                    // if we're at the end of the line, stop
+                    if new_y == merge_limits[x] {
+                        break;
+                    }
+
+                    // otherwise set up the next loop
+                    new_y += 1;
                 }
             }
         }
---
+++ src/game_structs/tests.rs
@@ -0,0 +1,230 @@
+use crate::game_structs::{GameState, Move};
+use crate::game_traits::{AddRandomPiece, FullGame};
+
+/// Test fixture helper which allows movement with no additional placement afterward
+struct NoPlacement {}
+
+impl<State: Clone + Sized> AddRandomPiece<State> for NoPlacement {
+    fn next_piece(&mut self, in_state: &State) -> State {
+        in_state.clone()
+    }
+}
+
+/// Test fixture helper which sweeps from top to bottom, left to right,
+/// and places a two in the first empty spot (if any)
+struct FirstPlaceTwos {}
+
+impl<const N: usize> AddRandomPiece<GameState<N>> for FirstPlaceTwos {
+    fn next_piece(&mut self, in_state: &GameState<N>) -> GameState<N> {
+        let mut out = in_state.clone();
+
+        for y in 0..N {
+            for x in 0..N {
+                if out.grid[y][x] == 0 {
+                    out.grid[y][x] = 2;
+                    return out;
+                }
+            }
+        }
+
+        // no placement possible
+        out
+    }
+}
+
+/// Test fixture helper which sweeps from top to bottom, left to right,
+/// and places a four in the first empty spot (if any)
+struct FirstPlaceFours {}
+
+impl<const N: usize> AddRandomPiece<GameState<N>> for FirstPlaceFours {
+    fn next_piece(&mut self, in_state: &GameState<N>) -> GameState<N> {
+        let mut out = in_state.clone();
+
+        for y in 0..N {
+            for x in 0..N {
+                if out.grid[y][x] == 0 {
+                    out.grid[y][x] = 4;
+                    return out;
+                }
+            }
+        }
+
+        // no moves possible
+        out
+    }
+}
+
+fn boring_grid() -> GameState<5> {
+    GameState {
+        grid: [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 2, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ],
+    }
+}
+
+fn crowded_grid_a() -> GameState<4> {
+    GameState {
+        grid: [[2, 2, 0, 4], [2, 2, 2, 0], [8, 4, 4, 4], [16, 4, 8, 4]],
+    }
+}
+
+#[test]
+fn test_move_left_boring() {
+    let mut rng = NoPlacement {};
+
+    let start = boring_grid();
+
+    let actual = start.apply_move(Move::Left, &mut rng).unwrap();
+
+    let expected = GameState {
+        grid: [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [2, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ],
+    };
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_move_left_crowded() {
+    let mut rng = NoPlacement {};
+
+    let start = crowded_grid_a();
+
+    let actual = start.apply_move(Move::Left, &mut rng).unwrap();
+
+    let expected = GameState {
+        grid: [[4, 4, 0, 0], [4, 2, 0, 0], [8, 8, 4, 0], [16, 4, 8, 4]],
+    };
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_move_right_boring() {
+    let mut rng = NoPlacement {};
+
+    let start = boring_grid();
+
+    let actual = start.apply_move(Move::Right, &mut rng).unwrap();
+
+    let expected = GameState {
+        grid: [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 2],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ],
+    };
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_move_right_crowded() {
+    let mut rng = NoPlacement {};
+
+    let start = crowded_grid_a();
+    let actual = start.apply_move(Move::Right, &mut rng).unwrap();
+
+    let expected = GameState {
+        grid: [
+            [0, 0, 4, 4],
+            [0, 0, 2, 4],
+            [0, 8, 4, 8],
+            [16, 4, 8, 4],
+        ]
+    };
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_move_up_boring() {
+    let mut rng = NoPlacement {};
+
+    let start = boring_grid();
+
+    let actual = start.apply_move(Move::Up, &mut rng).unwrap();
+
+    let expected = GameState {
+        grid: [
+            [0, 0, 2, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ],
+    };
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_move_up_crowded() {
+    let mut rng = NoPlacement {};
+
+    let start = crowded_grid_a();
+
+    let actual = start.apply_move(Move::Up, &mut rng).unwrap();
+
+    let expected = GameState {
+        grid: [
+            [4, 4, 2, 8],
+            [8, 8, 4, 4],
+            [16, 0, 8, 0],
+            [0, 0, 0, 0],
+        ]
+    };
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_move_down_boring() {
+    let mut rng = NoPlacement {};
+
+    let start = boring_grid();
+
+    let actual = start.apply_move(Move::Down, &mut rng).unwrap();
+
+    let expected = GameState {
+        grid: [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 2, 0, 0],
+        ],
+    };
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_move_down_crowded() {
+    let mut rng = NoPlacement {};
+
+    let start = crowded_grid_a();
+    let actual = start.apply_move(Move::Down, &mut rng).unwrap();
+
+    let expected = GameState {
+        grid: [
+            [0, 0, 0, 0],
+            [4, 0, 2, 0],
+            [8, 4, 4, 4],
+            [16, 8, 8, 8],
+        ]
+    };
+
+    assert_eq!(actual, expected);
+}
